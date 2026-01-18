60c8cae63daceed4ff5d9983740eb46a7f9c40d3
devabhi96 <abhi1997shuklas@gmail.com>
2026-01-18 19:24:22 +0530

    Add files via upload

---
+++ conn.java
@@ -0,0 +1,20 @@
+package bank;
+
+import java.sql.*;
+
+public class conn{
+    Connection c;
+    Statement s;
+    public conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c =DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","Shuklaji@123");
+            s =c.createStatement();
+
+
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
